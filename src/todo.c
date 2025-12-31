845c6d825a857a77b25d94e4c60a69dc0d3389f6
zainyehia <zainyehia1@gmail.com>
2025-12-31 19:27:42 +0200

    Add store_tasks & save_tasks function & free_tasks & remove_tasks, Update list_tasks and main (add remove option and call free_tasks() at the end)

--- src/todo.c
+++ src/todo.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Task struct (will be used later)
+// Task struct
 typedef struct {
     int id;
     char *description;
 } Task;
 
-// Each file can only have a maximum of 50 tasks (used later)
+// Each file can only have a maximum of 50 tasks
 #define MAX_TASKS 50
 Task tasks[MAX_TASKS];
+
+int task_count = 0; // Track the number of tasks
 
 // Functions for every command
 // next_id helper function
@@ -39,7 +41,7 @@ int add_task(char *description, char *filename) {
 
     FILE *file = fopen(filename, "a");
     if (file != NULL) {
-        // if file is empty, add headers
+        // If file is empty, add headers
         if (id == 1) {
             fprintf(file,"Task Number\tTask Description\n");
         }
@@ -54,22 +56,103 @@ int add_task(char *description, char *filename) {
 }
 
 // List command
-int list_tasks(char *filename) {
+// Store the tasks into the "tasks" array using the helper function "store_tasks"
+void store_tasks(char *filename) {
     FILE *file = fopen(filename, "r");
-    if (file != NULL) {
-        char *line = NULL;
-        size_t len = 0;
-        while (getline(&line, &len, file) != -1) {
-            printf("%s", line);
+    if (file == NULL) {
+        return;
+    }
+
+    char line [256];
+    fgets(line, sizeof(line), file); // Skip the headers
+
+    for (int i = 0; fgets(line, sizeof(line), file) != NULL && i < MAX_TASKS; i++) {
+        char description[256];
+        sscanf(line, "%d\t%[^\n]", &tasks[i].id, description);
+
+        tasks[i].description = malloc(strlen(description) + 1); // Allocate memory for the task description with the size of the temporary description used above + the NUL character (\0)
+        if (tasks[i].description != NULL) {
+            strcpy(tasks[i].description, description);
+            task_count++;
         }
-        free(line);
-        fclose(file);
-        return 0;
+    }
+    fclose(file);
+}
+
+// Free dynamically allocated memory
+void free_tasks() {
+    // Free old memory
+    for (int i = 0; i < MAX_TASKS; i++) {
+        if (tasks[i].description != NULL) {
+            free(tasks[i].description);
+            tasks[i].description = NULL;
+        }
+    }
+}
+
+void list_tasks(char *filename) {
+    store_tasks(filename);
+
+    if (task_count == 0) {
+        printf("No tasks yet!\n");
+        printf("Add tasks using this command: `./todo add \"Task Description\" [file-name.tsv (keep empty if you want to write to the default file [todo.tsv])]\n");
     }
     else {
-        printf("Error reading from file! Terminating...\n");
+        printf("Your tasks\n");
+        printf("==========\n");
+        for (int i = 0; i < task_count; i++) {
+            printf("%d. %s\n", tasks[i].id, tasks[i].description);
+        }
+    }
+}
+
+int save_tasks(char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
         return 1;
     }
+
+    fprintf(file,"Task Number\tTask Description\n");
+
+    for (int i = 0; i < task_count; i++) {
+       fprintf(file,"%d\t%s\n", tasks[i].id, tasks[i].description);
+    }
+
+    return 0;
+}
+
+int remove_task(int id, char *filename) {
+
+    // Load tasks
+    store_tasks(filename);
+
+    if (id < 1 || id > task_count) {
+        printf("Invalid task number!\n");
+        printf("There are %d tasks in this list.\n", task_count);
+        return 1;
+    }
+
+    free(tasks[id - 1].description);
+
+    // Shift the array
+    for (int i = id - 1; i < task_count - 1; i++) {
+        tasks[i] = tasks[i + 1];
+    }
+
+    task_count--;
+
+    tasks[task_count].id = 0;
+    tasks[task_count].description = NULL;
+
+    // Update IDs
+    for (int i = 0; i < task_count; i++) {
+        tasks[i].id = i + 1;
+    }
+
+    // Save to the file again
+    save_tasks(filename);
+
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -94,15 +177,25 @@ int main(int argc, char *argv[]) {
             add_task(argv[2], "todo.tsv");
         }
 
-        if (strcmp(argv[1], "list") == 0) {
+        else if (strcmp(argv[1], "list") == 0) {
             list_tasks(argv[2]);
+        }
+
+        else if (strcmp(argv[1], "remove") == 0) {
+            remove_task(atoi(argv[2]), "todo.tsv");
         }
     }
     else if (argc == 4) {
         if (strcmp(argv[1], "add") == 0) {
             add_task(argv[2], argv[3]);
         }
+
+        else if (strcmp(argv[1], "remove") == 0) {
+            remove_task(atoi(argv[2]), argv[3]);
+        }
     }
+
+    free_tasks();
 
     return 0;
 }
